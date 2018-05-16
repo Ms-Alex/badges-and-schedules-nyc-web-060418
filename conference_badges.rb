1f0e4851593b0ba61dd7dd09a53bf052b4a74481
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-16 03:42:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  batch = []
+  for i in array_of_names
+    batch.push(badge_maker(i))
+  end
+  return batch
+end
+
+def assign_rooms(array_of_speakers)
+  messages = []
+  room = 1
+  
+end
+
+def printer (attendees)
+  print batch_badge_creator(attendees)
+end
