e37b1f17d9e8310ad9b06295d978e39e3d235e41
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-16 04:23:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,8 @@ def assign_rooms(array_of_speakers)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  for i in attendees
+    puts batch_badge_creator(attendees[i])
+    
   
 end
