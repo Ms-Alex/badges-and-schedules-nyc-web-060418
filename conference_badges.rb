61860a983c9ef65f99b534d1bebd19e7535ce988
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-16 03:56:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(array_of_speakers)
   messages = []
   room = 1
   (array_of_speakers).each_with_index do |name, i|
-    messages.push("Hello, #{name}! You'll be assigned to room #{i}!")
+    messages.push("Hello, #{name}! You'll be assigned to room #{i+1}!")
     room += 1
   end
   return messages
