beda3d961811af9c85504edd1a6b61aed864045a
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-16 04:12:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,10 +14,8 @@ end
 def assign_rooms(array_of_speakers)
   #messages = []
   array_of_speakers.collect.with_index do |name, i|
-  #collect.array_of_speakers.each_with_index do |name, i|
     "Hello, #{name}! You'll be assigned to room #{i+1}!"
   end
-  #return messages
 end
 
 def printer (attendees)
